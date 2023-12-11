0a9f661cedaff36db99cc7cfa61508fda055f4a3
Cristian Aldair Bonifacio Quispe <u20201b845@upc.edu.pe>
2023-12-11 11:50:07 -0500

    Add Controller
    add

--- Controllers/Frontend/trabajador/homeController.js
+++ Controllers/Frontend/trabajador/homeController.js
@@ -1,8 +1,14 @@
-exports.homeTrabajador = (req,res) =>{
+const DocCasos = require('../../../Models/Casos');
+
+exports.homeTrabajador = async (req,res) =>{
+    const DocumentosCasos = await DocCasos.findAll({
+        where: {userid: req.user.id}
+    })
     res.render('trabajador/home',{
         isHome: false,
         isCliente: false,
         isJobs: true,
         isAdmin: false,
+        DocumentosCasos:DocumentosCasos
 });
 }
