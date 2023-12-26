9176a27eee1ee411754b68f74076a6e888246694
nicolm2002 <151855461+nicolm2002@users.noreply.github.com>
2023-12-26 17:17:00 -0500

    Update homeController.js

--- Controllers/Frontend/public/homeController.js
+++ Controllers/Frontend/public/homeController.js
@@ -1,14 +1,15 @@
 const Noticias = require('../../../Models/Noticias');
 const Eventos= require('../../../Models/Eventos');
 const usuarios = require('../../../Models/Usuario');
-const blog=require('../../../Models/Blogs');
-exports.home = (req,res) =>{
+exports.home = async (req,res) =>{
+    const noticias = await Noticias.findAll({ include: usuarios });
     res.render('public/home',{
         isHome: true,
         isCliente: false,
         isJobs: false,
         isAdmin: false,
-        isFooter: true
+        isFooter: true,
+        noticias: noticias,
 });
 }
 exports.service = (req,res) =>{
@@ -72,15 +73,13 @@ exports.About = (req,res) =>{
         isFooter: true
     })
 }
-exports.Blogs = async (req,res) =>{
-    const blogs = await blog.findAll({ include: usuarios }); // Asegúrate de incluir la relación con la tabla de usuarios
+exports.Blogs = (req,res) =>{
     res.render('public/blogs',{
         isHome: true,
         isCliente: false,
         isJobs: false,
         isAdmin: false,
-        isFooter: true,
-        blogs : blogs,
+        isFooter: true
     })
 }
 exports.Eventos = async (req,res) =>{
@@ -94,41 +93,6 @@ exports.Eventos = async (req,res) =>{
         eventos:eventos,
     })
 }
-exports.blogdetail = async (req, res) => {
-    // Obtener el ID de la noticia desde los parámetros de la URL
-    const blogid = req.params.id;
-    const blogs= await blog.findByPk(blogid, {
-        include: usuarios,
-      });    // Lógica para obtener los detalles de la noticia con el ID proporcionado
-    // Esta lógica dependerá de cómo recuperas los datos de la noticia en tu aplicación
-
-    // Renderizar la vista de detalle de la noticia (por ejemplo, 'detalleNoticia.ejs')
-    res.render('public/blog-detail', {     isHome: true,
-        isCliente: false,
-        isJobs: false,
-        isAdmin: false,
-        isFooter: true,
-        blogs }); // Pasar el ID de la noticia a la vista
-}
-
-
-exports.eventoDetail = async (req, res) => {
-    // Obtener el ID de la noticia desde los parámetros de la URL
-    const eventoId = req.params.id;
-    const evento= await Eventos.findByPk(eventoId, {
-        include: usuarios,
-      });    // Lógica para obtener los detalles de la noticia con el ID proporcionado
-    // Esta lógica dependerá de cómo recuperas los datos de la noticia en tu aplicación
-
-    // Renderizar la vista de detalle de la noticia (por ejemplo, 'detalleNoticia.ejs')
-    res.render('public/evento-detail', {     isHome: true,
-        isCliente: false,
-        isJobs: false,
-        isAdmin: false,
-        isFooter: true,
-        evento }); // Pasar el ID de la noticia a la vista
-}
-
 exports.home6 = (req,res) =>{
     res.render('public/home6',{
         isHome: false,
